a646f5e401b61d7604b6511e35ecc442319a2726
D4vidson <davidsonsampaio08@gmail.com>
2025-03-25 11:03:58 -0300

    Adicionar arquivos de projeto.

---
+++ WebPiagetMvcDG/Models/Aluno.cs
@@ -0,0 +1,12 @@
+﻿namespace WebPiagetMvcDG.Models
+{
+    public class Aluno
+    {
+
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string Professor { get; set; }
+        public DateTime Idade { get; set; }
+  
+    }
+}
